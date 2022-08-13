c54c4bbb0ee29d0583d8644ecc4d0fe595660d1b
Brent Oates <brentoates@live.co.uk>
2022-08-13 11:30:54 +0100

    Allow announcements channel to be of News type

--- src/utilities/ConfigManager.ts
+++ src/utilities/ConfigManager.ts
@@ -12,7 +12,7 @@ export class ConfigManager {
 			messageContent = 'Please tag the channel.';
 		} else if (!chan) {
 			messageContent = 'Channel not found, or I do not have permission to access it.';
-		} else if (chan.type !== ChannelType.GuildText) {
+		} else if (chan.type !== ChannelType.GuildText && chan.type !== ChannelType.GuildNews) {
 			messageContent = 'Channel must be a Text Channel.';
 		} 
 
