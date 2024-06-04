3fcf2076a2b754d54bff8e3d2877ecdcdcb774d2
Brent Oates <3065898+BrentOates@users.noreply.github.com>
2024-06-04 07:54:40 +0000

    Update deps and fix DiscordJs change

--- src/types/SlashCommand.ts
+++ src/types/SlashCommand.ts
@@ -1,13 +1,14 @@
 import { 
     ChatInputCommandInteraction, 
     SlashCommandBuilder, 
+    SlashCommandOptionsOnlyBuilder, 
     SlashCommandSubcommandBuilder, 
     SlashCommandSubcommandGroupBuilder, 
     SlashCommandSubcommandsOnlyBuilder} from 'discord.js';
 import { ServerConfig } from '../client/models/ServerConfig';
 
 export interface SlashCommand {
-	commandData: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+	commandData: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder;
     execute(interaction: ChatInputCommandInteraction, config: ServerConfig): Promise<any>
 }
 
